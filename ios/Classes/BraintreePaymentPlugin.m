11b6dfded4758f2010130cf05d27a1a7d5d24440
Adarsh Kumar <kadarsh.singh99@gmail.com>
2019-06-07 11:06:17 +0530

    added ios

--- ios/Classes/BraintreePaymentPlugin.m
+++ ios/Classes/BraintreePaymentPlugin.m
@@ -1,20 +1,57 @@
 #import "BraintreePaymentPlugin.h"
+#import "BraintreeCore.h"
+#import "BraintreeDropIn.h"
+
+@interface BraintreePaymentPlugin ()
+    @property (nonatomic, strong) UIViewController *viewController;
+@end
 
 @implementation BraintreePaymentPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"braintree_payment"
             binaryMessenger:[registrar messenger]];
-  BraintreePaymentPlugin* instance = [[BraintreePaymentPlugin alloc] init];
+  UIViewController *viewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+  BraintreePaymentPlugin *instance = [[BraintreePaymentPlugin alloc] initWithViewController:viewController];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
+- (instancetype)initWithViewController:(UIViewController *)viewController {
+    self = [super init];
+    
+    if (self) {
+        _viewController = viewController;
+    }
+    
+    return self;
+}
+
+
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"showDropIn" isEqualToString:call.method]) {
+     NSString *clientToken = call.arguments[@"clientToken"];
+     [self showDropIn:clientToken withResult:result];
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (void)showDropIn:(NSString *)clientTokenOrTokenizationKey withResult:(FlutterResult)flutterResult {
+    BTDropInRequest *request = [[BTDropInRequest alloc] init];
+    BTDropInController *dropInController = [[BTDropInController alloc] initWithAuthorization:clientTokenOrTokenizationKey request:request handler:^(BTDropInController * _Nonnull controller, BTDropInResult * _Nullable result, NSError * _Nullable error) {
+        
+        if (error != nil) {
+            flutterResult([FlutterError errorWithCode:[NSString stringWithFormat:@"%li", error.code] message:error.localizedDescription details:nil]);
+        } else if (result.cancelled) {
+            flutterResult([FlutterError errorWithCode:@"CANCELLED" message:@"Purchase process was cancelled." details:nil]);
+        } else {
+            flutterResult(result.paymentMethod.nonce);
+        }
+        
+        [self.viewController dismissViewControllerAnimated:YES completion:nil];
+    }];
+    
+    [_viewController presentViewController:dropInController animated:YES completion:nil];
 }
 
 @end
